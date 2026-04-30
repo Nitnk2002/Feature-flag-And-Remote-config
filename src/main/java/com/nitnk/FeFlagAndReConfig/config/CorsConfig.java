5b38515c9f698716cb3ba4a7f86e106f091a6850
Nitnk2002 <nitmiddle@gmail.com>
2026-05-01 02:23:49 +0530

    cors added for safety!

---
+++ src/main/java/com/nitnk/FeFlagAndReConfig/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.nitnk.FeFlagAndReConfig.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        // Use OriginPatterns, NOT Origins, because we are using credentials!
+        configuration.setAllowedOriginPatterns(List.of("*"));
+
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // Apply this completely open CORS policy to absolutely every endpoint
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
